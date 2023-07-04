a1810ae7657bb2e5bad90b8b48dd401395a6e949
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-07-04 15:15:15 -0300

    estrutura do Scaffold

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,11 @@
+import 'package:alubank/components/sections/header.dart';
 import 'package:flutter/material.dart';
-
 
 class Home extends StatelessWidget {
 const Home({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context){
-    return Scaffold();
+    return Scaffold(body: Header(),);
   }
 }
