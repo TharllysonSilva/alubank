660c5c98af4076593d4a6a64e39ff1040a7ebc57
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-07-04 15:38:32 -0300

    chamando as propriedades do header

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,6 +6,10 @@ const Home({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context){
-    return const Scaffold(body: Header(),);
+    return const Scaffold(body: Column(
+      children: <Widget>[
+        Header(),
+      ],
+    ),);
   }
 }
