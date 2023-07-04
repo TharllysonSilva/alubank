5f4bbf66b25d45dde8821fdd92c8835d330569a9
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-07-04 12:22:19 -0300

    estrutura do projeto

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Alubank());
+}
+
+class Alubank extends StatelessWidget {
+  const Alubank({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '',
+    );
+  }
+}
