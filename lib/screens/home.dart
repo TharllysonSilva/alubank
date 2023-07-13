a1f500c1eb954ddc2fa29154dea6c05ebd3d4b10
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-07-13 16:00:52 -0300

    refatorando o home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:alubank/components/box_card.dart';
 import 'package:alubank/components/sections/header.dart';
 import 'package:flutter/material.dart';
 
@@ -6,9 +7,10 @@ const Home({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context){
-    return const Scaffold(body: Column(
+    return Scaffold(body: Column(
       children: <Widget>[
         Header(),
+        BoxCard(boxContent: Text('Olá mundo')),
       ],
     ),);
   }
